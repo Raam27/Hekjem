b8c47d3aaa6cf9afaec95b6b3f301b71ca3fa1ed
Raam27 <45362796+Raam27@users.noreply.github.com>
2020-07-14 21:52:45 +0800

    basic
    basic

---
+++ Learning/Assets/Script/Enemy.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public GameObject player;
+    public Rigidbody2D r2d;
+    public float distance;
+    public int speed;
+    private float latestDirectionChangeTime;
+    private readonly float directionChangeTime = 3f;
+    private float patrolSpeed = 1f;
+    private Vector2 movementDirection;
+    private Vector2 movementPerSecond;
+    public float xMin, xMax, yMin, yMax;
+    // Start is called before the first frame update
+    void Start()
+    {
+        r2d = GetComponent<Rigidbody2D>();
+        latestDirectionChangeTime = 0f;
+        move();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player != null)
+        {
+            distance = Vector2.Distance(player.transform.position, gameObject.transform.position);
+            chase();
+            if (distance > 6)
+            {
+                player = null;
+            }
+        }
+        else if (player == null)
+        {
+            if (Time.time - latestDirectionChangeTime > directionChangeTime)
+            {
+                latestDirectionChangeTime = Time.time;
+                move();
+            }
+            Vector2 gerak = new Vector2(transform.position.x + (movementPerSecond.x * Time.deltaTime),
+                transform.position.y + (movementPerSecond.y * Time.deltaTime));
+            transform.position = new Vector2(Mathf.Clamp(gerak.x, xMin, xMax), Mathf.Clamp(gerak.y, yMin, yMax));
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            player = collision.gameObject;
+        }
+    }
+
+    private void rotate()
+    {
+        Vector2 direction = new Vector2(player.transform.position.x - transform.position.x, player.transform.position.y - transform.position.y);
+        transform.up = direction;
+    }
+    private void chase()
+    {
+        transform.position = Vector2.MoveTowards(transform.position,player.transform.position,speed*Time.deltaTime);
+        rotate();
+    }
+    private void move()
+    {
+        movementDirection = new Vector2(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f)).normalized;
+        movementPerSecond = movementDirection * patrolSpeed;
+    }
+}
---
+++ Learning/Assets/Script/Movement.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Movement : MonoBehaviour
+{
+    
+    public int health = 100;
+    [SerializeField]
+    int rotationSpeed;
+    private Rigidbody2D r2d;
+    public int runSpeed;
+    public int walkSpeed;
+    public CircleCollider2D cc2d;
+
+    float horizontalInput;
+    float verticalInput;
+    float rotateInput;
+    Vector3 worldPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        r2d = GetComponent<Rigidbody2D>();
+        cc2d = gameObject.GetComponent<CircleCollider2D>(); 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        rotatePlayer();
+    }
+    private void FixedUpdate()
+    {
+        movePlayer();
+        if (horizontalInput == 0 && verticalInput == 0)
+        {
+            cc2d.radius = 0f;
+        }
+    }
+    private void movePlayer()
+    {
+        if (Input.GetButton("Run"))
+        {
+            r2d.velocity = new Vector2(horizontalInput * runSpeed, verticalInput * runSpeed);
+            cc2d.radius = 1.5f;
+        }
+        else 
+        {
+            r2d.velocity = new Vector2(horizontalInput * walkSpeed, verticalInput * walkSpeed);
+            cc2d.radius = 1.1f;
+        }
+    }
+    private void rotatePlayer()
+    {
+        //float rotation = worldPosition* rotationSpeed;
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition = Camera.main.ScreenToWorldPoint(mousePosition);
+
+        Vector2 direction = new Vector2(mousePosition.x - transform.position.x, mousePosition.y - transform.position.y);
+        transform.up = direction;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Enemy")
+        {
+            Destroy(gameObject);
+        }
+    }
+}
